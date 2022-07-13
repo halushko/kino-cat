6e52c316949c0280f0dacd20e605397556b3cb75
galushko <galushko@netcracker.com>
2022-07-13 22:53:57 +0300

    Fix build

--- fileConsumer/src/main/java/com/halushko/Main.java
+++ fileConsumer/src/main/java/com/halushko/Main.java
@@ -5,9 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world! I'm textConsumer!");
-//        System.out.println(ScriptCollectionElement.getCommand("/pause_1").getScript());
-//        System.out.println(ScriptCollectionElement.getCommand("/pause").getScript());
-//        System.out.println(ScriptCollectionElement.getCommand("/pause_1_2").getScript());
         UserMessageHandler handler = new UserMessageHandler();
         for(;;) {
             try {
