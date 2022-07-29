1542d4872d5723239220e6525492a6a124326e9b
galushko <galushko@netcracker.com>
2022-07-29 22:56:27 +0300

    Start implement TorrentOperator

--- torrent/src/main/java/com/halushko/Main.java
+++ torrent/src/main/java/com/halushko/Main.java
@@ -5,18 +5,6 @@ import com.halushko.rabKot.handlers.input.InputMessageHandler;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! I'm torrent!");
-        TorrentOperator handler = new TorrentOperator();
-        for(;;) {
-            try {
-                Thread.sleep(InputMessageHandler.LONG_PAUSE_MILIS);
-                handler.run();
-            } catch (Exception e) {
-                e.printStackTrace();
-                try {
-                    Thread.sleep(InputMessageHandler.MEDIUM_PAUSE_MILIS);
-                } catch (InterruptedException ignored) {
-                }
-            }
-        }
+        new TorrentOperator().run();
     }
 }
--- torrent/src/main/java/com/halushko/TorrentOperator.java
+++ torrent/src/main/java/com/halushko/TorrentOperator.java
@@ -6,34 +6,11 @@ import com.halushko.rabKot.cli.ExecuteBash;
 import com.halushko.rabKot.rabbit.RabbitUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TorrentOperator extends InputMessageHandler {
     public static final String EXECUTE_TORRENT_COMMAND_QUEUE = System.getenv("EXECUTE_TORRENT_COMMAND_QUEUE");
     public static final String TELEGRAM_OUTPUT_TEXT = System.getenv("TELEGRAM_OUTPUT_TEXT_QUEUE");
-
-//    static {
-//        String str = "EXECUTE_TORRENT_COMMAND_QUEUE";
-//        try {
-//            String str1 = System.getenv("EXECUTE_TORRENT_COMMAND_QUEUE");
-//            if (!(str1 == null || str1.equals("") || str1.equalsIgnoreCase("null"))) {
-//                str = str1;
-//            }
-//        } catch (Exception ignore) {
-//        }
-//        EXECUTE_TORRENT_COMMAND_QUEUE = str;
-//    }
-//    static {
-//        String str = "TELEGRAM_OUTPUT_TEXT";
-//        try {
-//            String str1 = System.getenv("TELEGRAM_OUTPUT_TEXT_QUEUE");
-//            if (!(str1 == null || str1.equals("") || str1.equalsIgnoreCase("null"))) {
-//                str = str1;
-//            }
-//            str = str1;
-//        } catch (Exception ignore) {
-//        }
-//        TELEGRAM_OUTPUT_TEXT = str;
-//    }
 
     @Override
     protected void getDeliverCallbackPrivate(RabbitMessage rabbitMessage) {
@@ -42,7 +19,8 @@ public class TorrentOperator extends InputMessageHandler {
             long userId = rabbitMessage.getUserId();
 
             List<String> result = ExecuteBash.executeViaCLI(text);
-            RabbitUtils.postMessage(userId, text + " !NOT EXECUTED. TODO!", TELEGRAM_OUTPUT_TEXT);
+            String textResult = result.stream().map(a -> a + "\n").collect(Collectors.joining());
+            RabbitUtils.postMessage(userId, textResult, TELEGRAM_OUTPUT_TEXT);
         } catch (Exception e) {
             System.out.println("During message handle got an error: " + e.getMessage());
         }
