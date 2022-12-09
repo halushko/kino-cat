c1b3ab8e3afcbacca05951749f3245607253eb35
Dmytro Galushko <galushko@netcracker.com>
2022-12-09 16:13:16 +0200

    Add log

--- middleware/src/main/java/com/halushko/kinocat/middleware/handlers/input/PrivateCliCommandExecutor.java
+++ middleware/src/main/java/com/halushko/kinocat/middleware/handlers/input/PrivateCliCommandExecutor.java
@@ -2,12 +2,15 @@ package com.halushko.kinocat.middleware.handlers.input;
 
 import com.halushko.kinocat.middleware.cli.ExecuteBash;
 import com.halushko.kinocat.middleware.cli.ScriptsCollection;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
 public abstract class PrivateCliCommandExecutor {
     protected List<String> execute(String arg) {
-        return ExecuteBash.executeViaCLI(getScriptsCollection().getCommand(String.format("%s%s", getScript(), arg)).getFinalCommand());
+        String text = String.format("%s%s", getScript(), arg);
+        Logger.getRootLogger().debug(String.format("[PrivateCliCommandExecutor.execute] %s", text));
+        return ExecuteBash.executeViaCLI(getScriptsCollection().getCommand(text).getFinalCommand());
     }
 
     protected abstract String getScript();
