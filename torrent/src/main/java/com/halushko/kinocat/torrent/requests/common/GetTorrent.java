a8d64c14ed9a4ef82871209fd4ecc752999ed699
galushko <galushko@netcracker.com>
2023-10-31 20:31:53 +0200

    Add start and pause all command Change CommandsCollection Move Void actions to the separate classes Refactoring Fix percentage for file list

--- torrent/src/main/java/com/halushko/kinocat/torrent/requests/common/GetTorrent.java
+++ torrent/src/main/java/com/halushko/kinocat/torrent/requests/common/GetTorrent.java
@@ -4,13 +4,12 @@ import com.halushko.kinocat.core.rabbit.SmartJson;
 import com.halushko.kinocat.torrent.entities.TorrentEntity;
 
 import java.util.Comparator;
+import java.util.List;
 
 public abstract class GetTorrent extends TransmissionWebApiExecutor {
     @Override
-    protected final String executeRequest(SmartJson json) {
-        return String.join(
-                "\n",
-                json.getSubMessage("OUTPUT")
+    protected final List<String> executeRequest(SmartJson json) {
+        return json.getSubMessage("OUTPUT")
                         .getSubMessage("arguments")
                         .getSubMessage("torrents")
                         .convertToList()
@@ -18,8 +17,7 @@ public abstract class GetTorrent extends TransmissionWebApiExecutor {
                         .map(TorrentEntity::new)
                         .sorted(Comparator.comparing(TorrentEntity::getName))
                         .map(this::generateAnswer)
-                        .toList()
-        );
+                        .toList();
     }
 
     protected abstract String generateAnswer(TorrentEntity torrent);
--- torrent/src/main/java/com/halushko/kinocat/torrent/requests/common/TransmissionWebApiExecutor.java
+++ torrent/src/main/java/com/halushko/kinocat/torrent/requests/common/TransmissionWebApiExecutor.java
@@ -9,6 +9,8 @@ import com.halushko.kinocat.core.web.InputMessageHandlerApiRequest;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
+import java.util.List;
+
 // https://github.com/transmission/transmission/blob/main/docs/rpc-spec.md
 @Slf4j
 public abstract class TransmissionWebApiExecutor extends InputMessageHandlerApiRequest {
@@ -47,14 +49,34 @@ public abstract class TransmissionWebApiExecutor extends InputMessageHandlerApiR
         log.debug("[executeRequest] Responce body:\n{}", responceBody);
         SmartJson json = new SmartJson(SmartJson.KEYS.INPUT, message.getRabbitMessageText()).addValue(SmartJson.KEYS.OUTPUT, responceBody);
 
-        String output = isResultValid(json)
-                ? executeRequest(json)
-                : String.format("result of request is: %s", responceBody);
-
-        RabbitUtils.postMessage(chatId, output, Constants.Queues.Telegram.TELEGRAM_OUTPUT_TEXT);
+        if (isResultValid(json)) {
+            val result = executeRequest(json);
+            int i = 0;
+            int j = 0;
+            boolean flag = false;
+            StringBuilder sb = new StringBuilder("Торенти 01-09\n");
+            for (String answer : result) {
+                i++;
+                if (i < 10) {
+                    sb.append(answer).append("\n");
+                    flag = true;
+                } else {
+                    RabbitUtils.postMessage(chatId, sb.toString(), Constants.Queues.Telegram.TELEGRAM_OUTPUT_TEXT);
+                    flag = false;
+                    i = 0;
+                    j++;
+                    sb = new StringBuilder("Торенти ").append(j * 10).append("-").append(j * 11 < result.size() ? j * 10 + 9 : result.size());
+                }
+            }
+            if(flag) {
+                RabbitUtils.postMessage(chatId, sb.toString(), Constants.Queues.Telegram.TELEGRAM_OUTPUT_TEXT);
+            }
+        } else {
+            RabbitUtils.postMessage(chatId, String.format("result of request is: %s", responceBody), Constants.Queues.Telegram.TELEGRAM_OUTPUT_TEXT);
+        }
     }
 
-    protected abstract String executeRequest(SmartJson message);
+    protected abstract List<String> executeRequest(SmartJson message);
 
     protected boolean isResultValid(SmartJson result) {
         return "success".equalsIgnoreCase(result.getSubMessage(SmartJson.KEYS.OUTPUT).getValue("result"));
