8e53adfd6302e0f056ff0a4489ec4f498533d90e
Dmytro Galushko <galushko@netcracker.com>
2024-03-30 21:25:02 +0200

    Add more then 2 storages

--- torrent/src/main/resources/scripts/run.sh
+++ torrent/src/main/resources/scripts/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-TORRENT_IP_ARRAY=$(echo $TORRENT_IP | jq -r '.[]')
+TORRENT_IP_ARRAY=$(echo "$TORRENT_IP" | jq -c '.[]')
 
-for ITEM in $TORRENT_IP_ARRAY
-do
-  NAME=$(echo $ITEM | jq -r '.name // empty')
+for ITEM in $TORRENT_IP_ARRAY; do
+  NAME=$(echo "$ITEM" | jq -r -c '.name // empty')
 
   if [ -n "$NAME" ]; then
     CONFIG_PATH="/home/app/transmission_config_$NAME"
@@ -12,14 +11,12 @@ do
     CONFIG_PATH="/home/app/transmission_config"
   fi
 
-  mkdir -p $CONFIG_PATH
+  mkdir -p "$CONFIG_PATH"
 
-  if [ -e $CONFIG_PATH/.unlock ]; then
-    echo "The transmission service was already configured by this service"
+  if [ -e "$CONFIG_PATH/.unlock" ]; then
+    echo "The transmission service for $NAME was already configured" > $CONFIG_PATH/.unlock
   else
     cp /home/app/settings.json $CONFIG_PATH/settings.json
-    echo "The transmission service configured by this service" > $CONFIG_PATH/.unlock
+    echo "The transmission service for <$NAME> configured by this service" > $CONFIG_PATH/.unlock
   fi
 done
-
-java -Dlog4j.configuration=file:/home/app/log4j-${LOGS_LEVEL}.properties -jar /home/app/app.jar
