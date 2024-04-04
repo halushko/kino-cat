7fd9811f41b6b1a6ca9b002750b743c21f67b1bf
Dmytro Halushko <dima.halushko@ukr.net>
2024-04-04 20:40:29 +0300

    1.18 (#49)
    - add auto generation of transmission configs after ENV files edition
    - fix /list_<number> commnd

--- torrent/src/main/resources/scripts/run.sh
+++ torrent/src/main/resources/scripts/run.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-mkdir -p /home/app/transmission_config
-if [ -e /home/app/transmission_config/.settings.lock ]; then
-  echo "The transmission service was already configured by this service"
-else
-  cp /home/app/settings.json /home/app/transmission_config/settings.json
-  echo "The transmission service configured by this service"
-  echo "The transmission service configured by this service" > /home/app/transmission_config/.settings.lock
-fi
+TORRENT_IP_ARRAY=$(echo "$TORRENT_IP" | jq -c '.[]')
 
-mkdir -p /home/app/transmission_config_hdd
-if [ -e /home/app/transmission_config_hdd/.settings.lock ]; then
-  echo "The transmission hdd service was already configured by this service"
-else
-  cp /home/app/settings.json /home/app/transmission_config_hdd/settings.json
-  echo "The transmission hdd service configured by this service"
-  echo "The transmission hdd service configured by this service" > /home/app/transmission_config_hdd/.settings.lock
-fi
+for ITEM in $TORRENT_IP_ARRAY; do
+  NAME=$(echo "$ITEM" | jq -r -c '.name // empty')
+
+  if [ -n "$NAME" ]; then
+    CONFIG_PATH="/home/app/transmission_config_$NAME"
+  else
+    CONFIG_PATH="/home/app/transmission_config"
+  fi
+
+  mkdir -p "$CONFIG_PATH"
+
+  if [ -e "$CONFIG_PATH/.unlock" ]; then
+    echo "The transmission service for <$NAME> was already configured" >> $CONFIG_PATH/.unlock
+  else
+    cp /home/app/settings.json $CONFIG_PATH/settings.json
+    echo "The transmission service for <$NAME> configured by this service" > $CONFIG_PATH/.unlock
+    chmod 777 -R "$CONFIG_PATH"/*
+  fi
+done
 
 java -Dlog4j.configuration=file:/home/app/log4j-${LOGS_LEVEL}.properties -jar /home/app/app.jar
